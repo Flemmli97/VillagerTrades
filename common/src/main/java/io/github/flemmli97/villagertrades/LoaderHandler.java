4d2122b9268ec654b36250d61183d6c4270978bc
Flemmli97 <Flemmli97@users.noreply.github.com>
2025-04-23 13:00:51 +0200

    move ftb ranks integration to common

--- common/src/main/java/io/github/flemmli97/villagertrades/LoaderHandler.java
+++ common/src/main/java/io/github/flemmli97/villagertrades/LoaderHandler.java
@@ -1,19 +1,26 @@
 package io.github.flemmli97.villagertrades;
 
+import dev.ftb.mods.ftbranks.api.FTBRanksAPI;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.server.level.ServerPlayer;
 
-import java.nio.file.Path;
-
 public interface LoaderHandler {
-
-    Path getConfigPath();
 
     default boolean hasPerm(CommandSourceStack src, String perm) {
         return this.hasPerm(src, perm, false);
     }
 
-    boolean hasPerm(CommandSourceStack src, String perm, boolean adminCmd);
+    default boolean hasPerm(CommandSourceStack src, String perm, boolean adminCmd) {
+        if (VillagerTrades.FTB_RANKS && src.getEntity() instanceof ServerPlayer player) {
+            return FTBRanksAPI.getPermissionValue(player, perm).asBoolean().orElse(src.hasPermission(!adminCmd ? 0 : 2));
+        }
+        return src.hasPermission(!adminCmd ? 0 : 2);
+    }
 
-    boolean hasPerm(ServerPlayer src, String perm, boolean adminCmd);
+    default boolean hasPerm(ServerPlayer player, String perm, boolean adminCmd) {
+        if (VillagerTrades.FTB_RANKS) {
+            return FTBRanksAPI.getPermissionValue(player, perm).asBoolean().orElse(player.hasPermissions(!adminCmd ? 0 : 2));
+        }
+        return player.hasPermissions(!adminCmd ? 0 : 2);
+    }
 }
