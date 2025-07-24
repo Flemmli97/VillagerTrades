32bc2ec49726ed3fe576d34b7b119246207de1e5
Flemmli97 <Flemmli97@users.noreply.github.com>
2025-07-24 18:25:39 +0200

    update 1.21.8

--- common/src/main/java/io/github/flemmli97/villagertrades/mixin/MerchantOfferMixin.java
+++ common/src/main/java/io/github/flemmli97/villagertrades/mixin/MerchantOfferMixin.java
@@ -2,6 +2,7 @@ package io.github.flemmli97.villagertrades.mixin;
 
 import io.github.flemmli97.villagertrades.helper.MerchantOfferMixinInterface;
 import net.minecraft.world.item.trading.MerchantOffer;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.Shadow;
@@ -20,6 +21,7 @@ public class MerchantOfferMixin implements MerchantOfferMixinInterface {
 
     @Shadow
     private int uses;
+    @Final
     @Shadow
     @Mutable
     private int maxUses;
